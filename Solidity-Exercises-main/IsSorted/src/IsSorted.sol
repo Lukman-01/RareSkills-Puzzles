1bd2df3d3f6ea1cd2d02e7188a36c22daecaaa2d
Lukman-01 <abdulyekeenlukman@gmail.com>
2024-07-08 13:30:52 +0100

    sort

--- Solidity-Exercises-main/IsSorted/src/IsSorted.sol
+++ Solidity-Exercises-main/IsSorted/src/IsSorted.sol
@@ -6,6 +6,20 @@ contract IsSorted {
      * The goal of this exercise is to return true if the members of "arr" is sorted (in ascending order) or false if its not.
      */
     function isSorted(uint256[] calldata arr) public view returns (bool) {
-        // your code here
+        // An empty array or a single element array is considered sorted
+        if (arr.length < 2) {
+            return true;
+        }
+
+        // Iterate through the array and compare each element with the next one
+        for (uint256 i = 0; i < arr.length - 1; i++) {
+            // If the current element is greater than the next element, array is not sorted
+            if (arr[i] > arr[i + 1]) {
+                return false;
+            }
+        }
+
+        // If no elements are out of order, the array is sorted
+        return true;
     }
 }
