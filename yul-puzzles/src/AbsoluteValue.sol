0ede7b654711d75c113dcc728501064a071441ff
Lukman-01 <abdulyekeenlukman@gmail.com>
2025-06-27 20:19:38 +0100

    passed

--- yul-puzzles/src/AbsoluteValue.sol
+++ yul-puzzles/src/AbsoluteValue.sol
@@ -5,11 +5,22 @@ contract AbsoluteValue{
 
   function main(int256 x) external pure returns (uint256) {
       assembly {
-          // your code here
-          // return the absolute value of x
-          // hint: use signed comparisons
-          // hint: https://www.rareskills.io/post/signed-int-solidity
-
+          // Check if x is negative by checking the sign bit
+          // If x < 0, we need to negate it
+          // We can use slt (signed less than) to compare with 0
+          
+          // Store x in a temporary variable
+          let temp := x
+          
+          // Check if x is negative (x < 0)
+          if slt(x, 0) {
+              // If negative, negate it: -x = ~x + 1 (two's complement)
+              temp := add(not(x), 1)
+          }
+          
+          // Return the absolute value
+          mstore(0x0, temp)
+          return(0x0, 0x20)
       }
   }
 }
