fc490b7df5d18352206b4a270086750d36934dd8
Lukman-01 <abdulyekeenlukman@gmail.com>
2025-08-02 23:17:54 +0100

    passed

--- yul-puzzles/src/DeployNBytes.sol
+++ yul-puzzles/src/DeployNBytes.sol
@@ -4,12 +4,46 @@ pragma solidity ^0.8.13;
 contract DeployNBytes {
     function main(uint256 size) external returns (address) {
         assembly {
-            // your code here
-            // create a contract that is size bytes long
-            // hint: you will need to generalize the init code of DeployOneByte
-            // hint: use mstore8 to target a single byte
-            // hint: because we only care about the size, you can simply return that region
-            //       of memory and not care about what is inside it
-       }
+            // Start writing init code at memory position 0
+            let memPtr := 0
+            
+            // We need to create init code that returns 'size' bytes
+            // The init code structure:
+            // 1. Push the size onto stack
+            // 2. Push 0 (offset) onto stack  
+            // 3. RETURN opcode
+            
+            // Store the runtime code (just zeros) starting after init code
+            // We'll put runtime code at offset that comes after our init code
+            let runtimeOffset := 0x20 // Start runtime code at offset 32
+            
+            // Fill the runtime code area with zeros (or any bytes)
+            for { let i := 0 } lt(i, size) { i := add(i, 1) } {
+                mstore8(add(runtimeOffset, i), 0x00)
+            }
+            
+            // Now create the init code that will return this runtime code
+            // PUSH2 size (3 bytes: 0x61 + 2 bytes for size)
+            mstore8(memPtr, 0x61) // PUSH2 opcode
+            mstore8(add(memPtr, 1), shr(8, size)) // high byte of size
+            mstore8(add(memPtr, 2), size) // low byte of size
+            
+            // PUSH1 runtimeOffset (2 bytes: 0x60 + 1 byte for offset)
+            mstore8(add(memPtr, 3), 0x60) // PUSH1 opcode
+            mstore8(add(memPtr, 4), runtimeOffset) // runtime offset
+            
+            // RETURN (1 byte: 0xf3)
+            mstore8(add(memPtr, 5), 0xf3) // RETURN opcode
+            
+            // Total init code length is 6 bytes
+            let initCodeSize := 6
+            
+            // Deploy the contract using CREATE
+            let addr := create(0, memPtr, add(initCodeSize, size))
+            
+            // Return the deployed contract address
+            mstore(0, addr)
+            return(0, 0x20)
+        }
     }
 }
