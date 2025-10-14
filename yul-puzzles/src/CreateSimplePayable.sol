1e6555faf66496fd242ddc214aadfdabdf9467f2
Lukman-01 <abdulyekeenlukman@gmail.com>
2025-10-14 13:08:41 +0100

    passed

--- yul-puzzles/src/CreateSimplePayable.sol
+++ yul-puzzles/src/CreateSimplePayable.sol
@@ -4,13 +4,20 @@ pragma solidity ^0.8.13;
 contract CreateSimplePayable {
     function main(uint256 dummy, bytes memory deploymentBytecode) external payable returns (address addr) {
         assembly {
-            // your code here
-            // create a contract using the deploymentBytecode
-            // return the address of the contract
-            // create it with the msg.value
-            // do this as part of create, don't transfer it after
-            // hint: use the `create` opcode
-            // hint: the bytecode is already in memory
+            // Get the length of the bytecode
+            let size := mload(deploymentBytecode)
+            
+            // Get the pointer to the actual bytecode data (skip the length prefix)
+            let dataPtr := add(deploymentBytecode, 0x20)
+            
+            // Get the value sent with this transaction
+            let value := callvalue()
+            
+            // Use CREATE opcode: create(value, offset, size)
+            // value = msg.value (ETH sent with deployment)
+            // offset = dataPtr (pointer to bytecode in memory)
+            // size = size (length of bytecode)
+            addr := create(value, dataPtr, size)
         }
     }
 }
