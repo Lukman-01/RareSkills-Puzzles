a68bab3048aa34b0b051c23c13de86fc4de3d520
Lukman-01 <abdulyekeenlukman@gmail.com>
2025-10-17 23:17:17 +0100

    passed

--- yul-puzzles/src/SetBit.sol
+++ yul-puzzles/src/SetBit.sol
@@ -5,9 +5,16 @@ contract SetBit {
 
   function main(uint256 x, uint8 i) external pure returns (uint256) {
       assembly {
-          // your code here
-          // set the i-th bit of x to 1, keeping every other bit the same
-          // return the result
+          // Create a mask with only the i-th bit set to 1
+          // 1 << i gives us a number with only the i-th bit set
+          let mask := shl(i, 1)
+          
+          // OR the mask with x to set the i-th bit to 1
+          let result := or(x, mask)
+          
+          // Return the result
+          mstore(0x00, result)
+          return(0x00, 0x20)
      }
   }
 }
