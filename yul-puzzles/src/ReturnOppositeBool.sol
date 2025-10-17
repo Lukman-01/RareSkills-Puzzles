a4e0aa96c28c8eb4f26da24a3d0ade1d870d45ca
Lukman-01 <abdulyekeenlukman@gmail.com>
2025-10-17 23:16:10 +0100

    passed

--- yul-puzzles/src/ReturnOppositeBool.sol
+++ yul-puzzles/src/ReturnOppositeBool.sol
@@ -4,8 +4,15 @@ pragma solidity ^0.8.13;
 contract ReturnOppositeBool {
     function main(bool _bool) external pure returns (bool) {
         assembly {
-            // your code here
-            // return the opposite of `_bool`
+            // Load the boolean value from calldata (it's at offset 4 after function selector)
+            let b := calldataload(4)
+            
+            // XOR with 1 to flip the boolean (0 becomes 1, 1 becomes 0)
+            let result := xor(b, 1)
+            
+            // Store result in memory and return
+            mstore(0x00, result)
+            return(0x00, 0x20)
         }
     }
 }
