13e7310dad9984a6e31d71095a04b9c90cd283e9
Lukman-01 <abdulyekeenlukman@gmail.com>
2025-10-25 22:10:59 +0100

    passed

--- yul-puzzles/src/ReturnTwoBools.sol
+++ yul-puzzles/src/ReturnTwoBools.sol
@@ -4,8 +4,17 @@ pragma solidity ^0.8.13;
 contract ReturnTwoBools {
     function main(bool a, bool b) external pure returns (bool, bool) {
         assembly {
-            // your code here
-            // return the tuple (a,b)
+            // Load bool a and b from calldata
+            // a is at offset 4, b is at offset 36 (4 + 32)
+            let valA := calldataload(4)
+            let valB := calldataload(36)
+            
+            // Store both values in memory
+            mstore(0x00, valA)
+            mstore(0x20, valB)
+            
+            // Return 64 bytes (2 * 32 bytes)
+            return(0x00, 0x40)
         }
     }
 }
