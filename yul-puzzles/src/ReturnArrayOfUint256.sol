67867b8cc40b07dda9032379a839aa78514c654c
Lukman-01 <abdulyekeenlukman@gmail.com>
2025-11-01 23:54:41 +0100

    passed

--- yul-puzzles/src/ReturnArrayOfUint256.sol
+++ yul-puzzles/src/ReturnArrayOfUint256.sol
@@ -4,8 +4,25 @@ pragma solidity ^0.8.13;
 contract ReturnArrayOfUint256 {
     function main(uint256 a, uint256 b, uint256 c) external pure returns (uint256[] memory) {
         assembly {
-            // your code here
-            // return an array of [a,b,c]
+            // Get free memory pointer
+            let ptr := mload(0x40)
+            
+            // Store the offset to array data (0x20)
+            mstore(ptr, 0x20)
+            
+            // Store array length (3)
+            mstore(add(ptr, 0x20), 3)
+            
+            // Store array elements
+            mstore(add(ptr, 0x40), a)
+            mstore(add(ptr, 0x60), b)
+            mstore(add(ptr, 0x80), c)
+            
+            // Update free memory pointer
+            mstore(0x40, add(ptr, 0xa0))
+            
+            // Return: offset(32) + length(32) + 3*elements(96) = 160 bytes (0xa0)
+            return(ptr, 0xa0)
         }
     }
 }
